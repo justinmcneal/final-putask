cb8d3104345a7ab724c28fbcf3376ada9f8c048f
chiyarrih <charrymaedoria@gmail.com>
2024-09-06 15:54:44 +0800

    fixed navigation menu

--- app/src/main/java/com/example/puttask/MainActivity.kt
+++ app/src/main/java/com/example/puttask/MainActivity.kt
@@ -34,7 +34,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
