ea8c16e7ca03506b8efba792a2f61a5665e43fad
chiyarrih <charrymaedoria@gmail.com>
2024-09-29 21:04:54 +0800

    doing recyclerview

--- app/src/main/java/com/example/puttask/Lists.kt
+++ app/src/main/java/com/example/puttask/Lists.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 
 class Lists : Fragment(R.layout.fragment_lists) {
 
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
