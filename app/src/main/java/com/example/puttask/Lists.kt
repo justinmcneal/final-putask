a1370d4c1d79505579663c2f5ffd8f5c88d0bfad
chiyarrih <charrymaedoria@gmail.com>
2024-09-30 00:59:07 +0800

    added recyclerview layoout

--- app/src/main/java/com/example/puttask/Lists.kt
+++ app/src/main/java/com/example/puttask/Lists.kt
@@ -5,9 +5,13 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Lists : Fragment(R.layout.fragment_lists) {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var listsAdapter: ListsAdapter
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,6 +31,23 @@ class Lists : Fragment(R.layout.fragment_lists) {
 
         // Set a custom background for the Spinner dropdown
         spinner.setPopupBackgroundResource(R.drawable.spinnerbg)
+
+        // Set up the RecyclerView
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // Sample data for the tasks
+        val taskList = listOf(
+            Task.Task("Title 1", "Description 1", "10:00 AM", false),
+            Task.Task("Title 2", "Description 2", "12:00 PM", true),
+            Task.Task("Title 3", "Description 3", "3:00 PM", false),
+            Task.Task("Title 4", "Description 4", "10:00 AM", false),
+            Task.Task("Title 5", "Description 5", "10:00 AM", false),
+            Task.Task("Title 6", "Description 6", "10:00 AM", false),)
+
+        // Initialize the adapter and set it to RecyclerView
+        listsAdapter = ListsAdapter(taskList)
+        recyclerView.adapter = listsAdapter
     }
 
 
