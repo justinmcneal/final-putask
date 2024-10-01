5b6ad227f0cb0b31b4802487aeeedbf5814ea6ba
JustinMcNeal <carononganjustinmcneal24@gmail.com>
2024-10-02 07:35:28 +0800

    Update AuthUtils.kt

--- app/src/main/java/com/example/puttask/api/AuthUtils.kt
+++ app/src/main/java/com/example/puttask/api/AuthUtils.kt
@@ -1,26 +1,26 @@
-package com.example.puttask.api
-
-import android.content.Context
-import android.content.SharedPreferences
-
-object AuthUtils {
-    private const val PREFS_NAME = "auth_prefs"
-    private const val TOKEN_KEY = "auth_token"
-
-    private var sharedPreferences: SharedPreferences? = null
-
-    fun init(context: Context) {
-        sharedPreferences = context.applicationContext.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-    }
-
-    fun storeToken(token: String) {
-        sharedPreferences?.edit()?.apply {
-            putString(TOKEN_KEY, token)
-            apply()
-        }
-    }
-
-    fun getToken(): String? {
-        return sharedPreferences?.getString(TOKEN_KEY, null)
-    }
-}
+//package com.example.puttask.api
+//
+//import android.content.Context
+//import android.content.SharedPreferences
+//
+//object AuthUtils {
+//    private const val PREFS_NAME = "auth_prefs"
+//    private const val TOKEN_KEY = "auth_token"
+//
+//    private var sharedPreferences: SharedPreferences? = null
+//
+//    fun init(context: Context) {
+//        sharedPreferences = context.applicationContext.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+//    }
+//
+//    fun storeToken(token: String) {
+//        sharedPreferences?.edit()?.apply {
+//            putString(TOKEN_KEY, token)
+//            apply()
+//        }
+//    }
+//
+//    fun getToken(): String? {
+//        return sharedPreferences?.getString(TOKEN_KEY, null)
+//    }
+//}
