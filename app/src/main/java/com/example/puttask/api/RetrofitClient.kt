9f21170e4b35e3d40a8842f3c73ea476fa44c768
LA-Reyes <163097815+LA-Reyes@users.noreply.github.com>
2024-10-04 23:25:50 +0800

    Added some shit

--- app/src/main/java/com/example/puttask/api/RetrofitClient.kt
+++ app/src/main/java/com/example/puttask/api/RetrofitClient.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.0.13:8000"
+    private const val BASE_URL = "http://192.168.68.102:8000"
 
     // Logging interceptor for seeing request/response in logcat
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
@@ -46,5 +46,13 @@ object RetrofitClient {
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ContactService::class.java)
+    }
+
+    val api: TaskService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(TaskService::class.java)
     }
 }
