754ecb3d8688f437ab5f89fa1289dda6d2dc45c9
LA-Reyes <163097815+LA-Reyes@users.noreply.github.com>
2024-10-04 23:28:17 +0800

    hehe

---
+++ app/src/main/java/com/example/puttask/AuthService.kt
@@ -0,0 +1,28 @@
+package com.example.puttask
+
+import com.example.puttask.api.LoginRequest
+import com.example.puttask.api.LoginResponse
+import com.example.puttask.api.RegistrationRequest
+import com.example.puttask.api.RegistrationResponse
+import com.example.puttask.api.User
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface AuthService {
+    @FormUrlEncoded
+    @POST("api/loginPost")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<LoginResponse>
+
+    @POST("api/registrationPost")
+    fun register(@Body registrationRequest: RegistrationRequest): Call<RegistrationResponse>
+
+    @GET("api/user")
+    fun getUser(): Call<User>
+}
