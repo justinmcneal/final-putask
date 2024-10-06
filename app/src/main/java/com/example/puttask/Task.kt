10536fc1a7b0ddaf29910874581c06677b376927
JustinMcNeal <carononganjustinmcneal24@gmail.com>
2024-10-06 09:02:30 +0800

    delete task.kt

--- app/src/main/java/com/example/puttask/Task.kt
+++
@@ -1,7 +0,0 @@
-//data class Task(
-//    val title: String,
-//    val description: String,
-//    val time: String,
-//    val isChecked: Boolean,
-//    val repeatDays: List<String> // Assuming this is a list of days
-//)
