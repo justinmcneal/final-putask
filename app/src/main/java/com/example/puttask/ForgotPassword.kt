309b96822fadbea6bdd50b833f57c542ceeb7b38
PeynBut <151439527+PeynBut@users.noreply.github.com>
2024-10-06 09:03:31 +0800

    add forget password profile and set ip adresss

---
+++ app/src/main/java/com/example/puttask/ForgotPassword.kt
@@ -0,0 +1,20 @@
+package com.example.puttask
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ForgotPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_forgot_password)
+
+
+
+
+
+               }
+}
