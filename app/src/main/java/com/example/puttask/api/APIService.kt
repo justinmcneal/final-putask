d237bb72f2f3fc5b1e2d20368524a548a46585fe
Justin Mc Neal <carononganjustinmcneal24@gmail.com>
2024-10-13 07:17:22 +0800

    aaa

--- app/src/main/java/com/example/puttask/api/APIService.kt
+++ app/src/main/java/com/example/puttask/api/APIService.kt
@@ -2,10 +2,12 @@ package com.example.puttask.api
 
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface APIService {
 
@@ -44,15 +46,15 @@ interface APIService {
     @POST("api/contact/send")
     suspend fun sendContactForm(@Body contactRequest: ContactRequest): Response<ContactResponse>
 
-//    // Create Task
-//    @POST("api/tasks")
-//    suspend fun createTask(@Body createRequest: CreateRequest): Response<CreateResponse>
-//
-//    // Update Task
-//    @PUT("api/tasks/{id}")
-//    suspend fun updateTask(@Path("id") id: Int, @Body updateRequest: UpdateRequest): Response<UpdateResponse>
-//
-//    // Delete Task
-//    @DELETE("api/tasks/{id}")
-//    suspend fun deleteTask(@Path("id") id: Int): Response<DeleteResponse>
+    // Create Task
+    @POST("api/tasks")
+    suspend fun createTask(@Body createRequest: CreateRequest): Response<CreateResponse>
+
+    // Update Task
+    @PUT("api/tasks/{id}")
+    suspend fun updateTask(@Path("id") id: Int, @Body updateRequest: UpdateRequest): Response<UpdateResponse>
+
+    // Delete Task
+    @DELETE("api/tasks/{id}")
+    suspend fun deleteTask(@Path("id") id: Int): Response<DeleteResponse>
 }
